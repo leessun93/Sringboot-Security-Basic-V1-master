520b714c4b12dce3887d8f2630236c8248aa0a83
leessun93 <dltjsgma@naver.com>
2024-04-25 16:53:57 +0900

    Initial commit

---
+++ java/com/cos/securityex01/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.cos.securityex01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration // IoC 빈(bean)을 등록
+@EnableWebSecurity // 필터 체인 관리 시작 어노테이션
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // 특정 주소 접근시 권한 및 인증을 위한 어노테이션 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	public BCryptPasswordEncoder encodePwd() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/user/**").authenticated()// /user/** 면 인증이 필요하고
+			//.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+			//.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN') and hasRole('ROLE_USER')") //admin이면
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll() //나머지는 누구나 들어갈 수 있다.
+		.and()
+			.formLogin()	//여기서부터는 manager admin user 쳐도 로긴패이지로 가도록 설정하는거
+			.loginPage("/login")
+			.loginProcessingUrl("/loginProc")
+			.defaultSuccessUrl("/");
+	}
+}
+
+
+
+
+
---
+++ src/main/resources/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.cos.securityex01.config;
+
+import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{  
+
+    //뷰 리졸버 오버라이딩,재설정
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+      MustacheViewResolver resolver = new MustacheViewResolver();
+
+      resolver.setCharset("UTF-8");
+      resolver.setContentType("text/html;charset=UTF-8");
+      resolver.setPrefix("classpath:/templates/");
+      resolver.setSuffix(".html");
+
+      registry.viewResolver(resolver);
+  }
+}
